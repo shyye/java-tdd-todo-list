66c5b32127bc59732837708c7058f5b9e4438c4c
shyye <emma.art.dev@gmail.com>
2024-08-15 12:19:22 +0200

    Add test for listAll()

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,6 +3,9 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 class TodoListTest {
 
     TodoList todoList;
@@ -13,5 +16,32 @@ class TodoListTest {
         Task task = new Task(1, "Code");
         Assertions.assertTrue(todoList.add(task));
         Assertions.assertEquals(1, todoList.size());
+    }
+
+    @Test
+    public void listAllShouldReturnList() {
+        // TODO: Not sure about this test, do I test the right things?
+
+        this.todoList = new TodoList();
+        Task task = new Task(1, "Code");
+        Task task2 = new Task(2, "Draw");
+
+        ArrayList<Task> correctlist = new ArrayList<>();
+        correctlist.add(task);
+        correctlist.add(task2);
+
+        ArrayList<Task> list = todoList.listAll();
+
+        Assertions.assertTrue(list.containsAll(correctlist));
+    }
+
+    @Test
+    public void listAllShouldReturnEmptyList() {
+        this.todoList = new TodoList();
+
+        ArrayList<Task> correctlist = new ArrayList<>();
+        ArrayList<Task> list = todoList.listAll();
+
+        Assertions.assertTrue(list.containsAll(correctlist));
     }
 }
