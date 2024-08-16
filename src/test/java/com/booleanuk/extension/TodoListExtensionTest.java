8da043e7e76abbe5c09906579f9f5f099876d243
shyye <emma.art.dev@gmail.com>
2024-08-16 14:33:06 +0200

    Add test for get task by id

--- src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
+++ src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
@@ -1,4 +1,21 @@
 package com.booleanuk.extension;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TodoListExtensionTest {
+    TodoListExtension todoList;
+
+    @Test
+    public void getTaskById() {
+        todoList = new TodoListExtension();
+        TaskExtension task = new TaskExtension(1, "Code a project");
+        todoList.add(task);
+
+        // Try to get task that exist
+        Assertions.assertTrue(task, todoList.getTask(1));
+
+        // Try to get task that dpesn't exist
+        Assertions.assertFalse(todoList.getTask(2));
+    }
 }
